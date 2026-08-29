3f4cd81b5f4a1a5f946f593b529f54905eb82bf4
tasheera <tasheeraabhishek@gmail.com>
2026-08-29 16:59:04 +0530

    Add GroqChatRequest DTO for handling chat request data structure

---
+++ Dtos/Groq/GroqChatRequest.cs
@@ -0,0 +1,28 @@
+using System.Text.Json.Serialization;
+
+namespace TicketTriage.Api;
+
+public record GroqChatRequest
+(
+    [property: JsonPropertyName("model")] string Model, // property: JsonPropertyName used for C# naming conventions(snake_case vs PascalCase)
+    [property: JsonPropertyName("messages")] List<GroqMessage> Messages,
+    [property: JsonPropertyName("response_format")] GroqResponseFormat ResponseFormat
+);
+
+
+public record GroqMessage(
+    [property: JsonPropertyName("role")] string Role,
+    [property: JsonPropertyName("content")] string Content
+);
+
+public record GroqResponseFormat(
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("json_schema")] GroqJsonSchema JsonSchema
+);
+
+public record GroqJsonSchema
+(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("strict")] bool Strict,
+    [property: JsonPropertyName("schema")] object Schema
+);
